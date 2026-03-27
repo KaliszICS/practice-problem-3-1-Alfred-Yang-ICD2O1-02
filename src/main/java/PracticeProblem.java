631cc62bcc64db4ca39c0f8df2b0845466d8ac35
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-03-27 15:25:52 +0000

    fwa

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,50 @@
+/**
+ * File: If statements
+ * Author: Alfred Yang
+ * Date Created: Mar 27, 2026
+ * Date Last Modified: Mar 27, 2026
+ */
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	}
+
+		public static String evenOrOdd(int num){
+			String result = "";
+			if (num % 2 == 0){
+				result = "Even";
+			}
+			else{
+				result = "Odd";
+			}
+			return result;
+		}
+
+		public static String teacherOrStudent(String duh){
+			String ka = "";
+			if (duh == "Kalisz"){
+				ka = "Teacher";
+			}
+			else{
+				ka = "Student";
+			}
+			return ka;}
+
+		public static int fartherFromZero(int buh){
+			int cuh = 0;
+			if (buh > 0){
+				cuh = buh + 5;
+			}
+			else if(buh < 0){
+				cuh = buh -5;
+			}
+			return cuh;
+
+
+
+		}
 
 	}
 
-}
+
